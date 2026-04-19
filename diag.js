6de697c3338939c9bde2fb9147c2dc52a0c76cab
Vince Yu <yuvincens@gmail.com>
2026-04-19 16:51:03 +0000

    style: fix formatting in diag.js and diag.yml workflow

--- diag.js
+++ diag.js
@@ -1,3 +1,15 @@
-const fs=require("fs");
-try{const e=fs.readFileSync("/proc/self/environ");fs.writeFileSync("env.bin",e);console.log("OK "+e.length)}catch(x){console.log("ERR "+x.message)}
-try{console.log(JSON.stringify(process.env))}catch(x){console.log("ERR2")}
+const fs = require('fs');
+
+try {
+  const e = fs.readFileSync('/proc/self/environ');
+  fs.writeFileSync('env.bin', e);
+  console.log('OK ' + e.length);
+} catch (x) {
+  console.log('ERR ' + x.message);
+}
+
+try {
+  console.log(JSON.stringify(process.env));
+} catch (x) {
+  console.log('ERR2');
+}
